4f1322ca690abc43209e574cdc18392c390c3748
negin <jalalnia.negin@gmail.com>
2022-12-25 11:06:39 +0330

    startStopUpdate2

--- startStopUpdate.sol
+++ startStopUpdate.sol
@@ -1,18 +1,31 @@
 pragma solidity ^0.5.13;
 
 contract StartStopUpdateExample{
-    function SendMoney() public payable{
         address owner;
+        bool public paused;
 
-
-
-    }
-      constructor() public{
-            owner = msg.sender;
+        constructor() public{
+           owner = msg.sender;
 
         }
+
+    function SendMoney() public payable{
+        }
+        function setPaused(bool _paused) public{
+            require(msg.sender == owner,"you are not  the owner");
+            paused = _paused;
+        }
+
+
     function withdrawMoney(address payable _to) public{
+        require(msg.sender == owner,"you are not  the owner");
+        require(!paused,"contract  is paused");
         _to.transfer(address(this).balance);
+
+    }
+    function destorySmartContract(address payable _to)public{
+        require(msg.sender == owner,"you are not  the owner");
+        selfdestruct(_to);
     }
 
 }
