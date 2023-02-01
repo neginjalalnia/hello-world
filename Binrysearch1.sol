fae7ce1d7c8ad8c2952b7a71e418dffb32fc8ab9
negin <jalalnia.negin@gmail.com>
2023-02-01 21:37:30 +0330

    BinarySearch

---
+++ Binrysearch1.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.17;
+
+
+contract BinarySearch{
+
+function binarysearch(uint[]calldata arr,uint l,uint r  ,uint x) public pure returns(uint){
+    if(r>=l){
+        uint middle =l+(r-l)/2;
+        if (arr[middle]==x){
+            return middle;
+             }
+            if(arr[middle]>x){
+                return binarysearch(arr,middle+1,l,x);
+            }
+            else if (arr [middle]<x){
+                 return  binarysearch(arr,middle-1,r,x);
+            }
+    }
+}
+
+}
